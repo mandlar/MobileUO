18e748ecdaee8d3aaec69c84a8d767296ced7f06
Bryan Denny <mandlar@gmail.com>
2025-06-27 16:10:51 -0400

    rolling forward to 8/28//2021 CUO commit b25ffac

--- Assets/Scripts/ClassicUO/src/IO/Resources/ArtLoader.cs
+++ Assets/Scripts/ClassicUO/src/IO/Resources/ArtLoader.cs
@@ -40,7 +40,6 @@ using ClassicUO.Game.Data;
 using ClassicUO.Renderer;
 using ClassicUO.Utility;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using SDL2;
 
 namespace ClassicUO.IO.Resources
@@ -56,7 +55,7 @@ namespace ClassicUO.IO.Resources
 
         private ArtLoader(int staticCount, int landCount) : base(staticCount)
         {
-            _graphicMask = Client.IsUOPInstallation ? (ushort) 0xFFFF : (ushort) 0x3FFF;
+            _graphicMask = Client.IsUOPInstallation ? (ushort)0xFFFF : (ushort)0x3FFF;
             _landResources = new UOTexture[landCount];
         }
 
@@ -74,7 +73,7 @@ namespace ClassicUO.IO.Resources
                     if (Client.IsUOPInstallation && File.Exists(filePath))
                     {
                         _file = new UOFileUop(filePath, "build/artlegacymul/{0:D8}.tga");
-                        Entries = new UOFileIndex[Math.Max(((UOFileUop) _file).TotalEntriesCount, Constants.MAX_STATIC_DATA_INDEX_COUNT)];
+                        Entries = new UOFileIndex[Math.Max(((UOFileUop)_file).TotalEntriesCount, Constants.MAX_STATIC_DATA_INDEX_COUNT)];
                     }
                     else
                     {
@@ -104,7 +103,7 @@ namespace ClassicUO.IO.Resources
 
             if (texture == null || texture.IsDisposed)
             {
-                ReadStaticArt(ref texture, (ushort) g);
+                ReadStaticArt(ref texture, (ushort)g);
 
                 if (texture != null)
                 {
@@ -130,7 +129,7 @@ namespace ClassicUO.IO.Resources
 
             if (texture == null || texture.IsDisposed)
             {
-                ReadLandArt(ref texture, (ushort) g);
+                ReadLandArt(ref texture, (ushort)g);
 
                 if (texture != null)
                 {
@@ -145,11 +144,13 @@ namespace ClassicUO.IO.Resources
             return texture;
         }
 
-        public unsafe IntPtr CreateCursorSurfacePtr(int index, ushort customHue, out short w, out short h)
+        public unsafe IntPtr CreateCursorSurfacePtr(int index, ushort customHue, out int hotX, out int hotY)
         {
+            hotX = hotY = 0;
+
             ref UOFileIndex entry = ref GetValidRefEntry(index + 0x4000);
 
-            if (ReadHeader(_file, ref entry, out w, out h))
+            if (ReadHeader(_file, ref entry, out short w, out short h))
             {
                 uint[] pixels = new uint[w * h];
 
@@ -159,13 +160,13 @@ namespace ClassicUO.IO.Resources
                     (
                         pixels,
                         ref entry,
-                        (ushort) index,
+                        (ushort)index,
                         w,
                         h,
                         out _
                     );
 
-                    fixed(uint * ptr = pixels)
+                    fixed (uint* ptr = pixels)
                     {
                         SDL.SDL_Surface* surface = (SDL.SDL_Surface*)SDL.SDL_CreateRGBSurfaceWithFormatFrom
                         (
@@ -177,44 +178,73 @@ namespace ClassicUO.IO.Resources
                             SDL.SDL_PIXELFORMAT_ABGR8888
                         );
 
-                        if (customHue > 0)
-                        {
-                            int stride = surface->pitch >> 2;
-                            uint* pixels_ptr = (uint*)surface->pixels;
-                            uint* p_line_end = pixels_ptr + w;
-                            uint* p_img_end = pixels_ptr + stride * h;
-                            int delta = stride - w;
-                            Color c = default;
+                        int stride = surface->pitch >> 2;
+                        uint* pixels_ptr = (uint*)surface->pixels;
+                        uint* p_line_end = pixels_ptr + w;
+                        uint* p_img_end = pixels_ptr + stride * h;
+                        int delta = stride - w;
+                        short curX = 0;
+                        short curY = 0;
+                        Color c = default;
 
-                            while (pixels_ptr < p_img_end)
+                        while (pixels_ptr < p_img_end)
+                        {
+                            curX = 0;
+
+                            while (pixels_ptr < p_line_end)
                             {
-                                while (pixels_ptr < p_line_end)
+                                if (*pixels_ptr != 0 && *pixels_ptr != 0xFF_00_00_00)
                                 {
-                                    if (*pixels_ptr != 0 && *pixels_ptr != 0xFF_00_00_00)
+                                    if (curX >= w - 1 || curY >= h - 1)
+                                    {
+                                        *pixels_ptr = 0;
+                                    }
+                                    else if (curX == 0 || curY == 0)
+                                    {
+                                        if (*pixels_ptr == 0xFF_00_FF_00)
+                                        {
+                                            if (curX == 0)
+                                            {
+                                                hotY = curY;
+                                            }
+
+                                            if (curY == 0)
+                                            {
+                                                hotX = curX;
+                                            }
+                                        }
+
+                                        *pixels_ptr = 0;
+                                    }
+                                    else if (customHue > 0)
                                     {
                                         c.PackedValue = *pixels_ptr;
                                         *pixels_ptr = HuesHelper.Color16To32(HuesLoader.Instance.GetColor16(HuesHelper.ColorToHue(c), customHue)) | 0xFF_00_00_00;
                                     }
-
-                                    ++pixels_ptr;
                                 }
 
-                                pixels_ptr += delta;
-                                p_line_end += stride;
+                                ++pixels_ptr;
+
+                                ++curX;
                             }
+
+                            pixels_ptr += delta;
+                            p_line_end += stride;
+
+                            ++curY;
                         }
 
                         return (IntPtr)surface;
                     }
                 }
             }
-            
+
             return IntPtr.Zero;
         }
 
         public bool PixelCheck(int index, int x, int y)
         {
-            return _picker.Get((ulong) index, x, y);
+            return _picker.Get((ulong)index, x, y);
         }
 
         public override bool TryGetEntryInfo(int entry, out long address, out long size, out long compressedSize)
@@ -343,21 +373,7 @@ namespace ClassicUO.IO.Resources
             int pos1 = 0;
             int minX = width, minY = height, maxX = 0, maxY = 0;
 
-            if (graphic >= 0x2053 && graphic <= 0x2062 || graphic >= 0x206A && graphic <= 0x2079)
-            {
-                for (int i = 0; i < width; i++)
-                {
-                    pixels[i] = 0;
-                    pixels[(height - 1) * width + i] = 0;
-                }
-
-                for (int i = 0; i < height; i++)
-                {
-                    pixels[i * width] = 0;
-                    pixels[i * width + width - 1] = 0;
-                }
-            }
-            else if (StaticFilters.IsCave(graphic) && ProfileManager.CurrentProfile != null && ProfileManager.CurrentProfile.EnableCaveBorder)
+            if (StaticFilters.IsCave(graphic) && ProfileManager.CurrentProfile != null && ProfileManager.CurrentProfile.EnableCaveBorder)
             {
                 AddBlackBorder(pixels, width, height);
             }
@@ -402,6 +418,22 @@ namespace ClassicUO.IO.Resources
                 {
                     if (ReadData(pixels, width, height, _file))
                     {
+                        // keep the cursor graphic check to cleanup edges
+                        if ((graphic >= 0x2053 && graphic <= 0x2062) || (graphic >= 0x206A && graphic <= 0x2079))
+                        {
+                            for (int i = 0; i < width; i++)
+                            {
+                                pixels[i] = 0;
+                                pixels[(height - 1) * width + i] = 0;
+                            }
+
+                            for (int i = 0; i < height; i++)
+                            {
+                                pixels[i * width] = 0;
+                                pixels[i * width + width - 1] = 0;
+                            }
+                        }
+
                         texture = new ArtTexture(width, height);
 
                         FinalizeData
@@ -479,7 +511,7 @@ namespace ClassicUO.IO.Resources
             texture = new UOTexture(44, 44);
             // we don't need to store the data[] pointer because
             // land is always hoverable
-            texture.SetDataPointerEXT(0, null, (IntPtr) data, SIZE * sizeof(uint));
+            texture.SetDataPointerEXT(0, null, (IntPtr)data, SIZE * sizeof(uint));
         }
 
         private void AddBlackBorder(Span<uint> pixels, int width, int height)
